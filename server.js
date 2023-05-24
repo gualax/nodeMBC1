4e18ef8f6b4bac1ced23ae2d68b4869c99d77fec
Alan Guala <guala.alan@gmail.com>
2023-05-24 12:47:06 +0200

    modificacion texto

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express  = require('express');
 
 const app = express();
 
-app.get('/', (req, res)=> res.send('Hello world con express'))
+app.get('/', (req, res)=> res.send('<h1>Express con html</h1>'))
 
 
 app.listen(3000);
