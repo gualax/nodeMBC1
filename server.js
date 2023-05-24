824ff20438a8ce56cdafc3dc64dfbaa173a86a27
Alan Guala <alanguala+3eo@ballastlane.com>
2023-05-24 12:18:47 +0200

    initial server

---
+++ server.js
@@ -0,0 +1,13 @@
+
+
+const express  = require('express');
+
+const app = express();
+
+app.get('/', (req, res)=> res.send('Hello world con express'))
+
+
+app.listen(3000);
+console.log("Server on port 3000")
+
+
